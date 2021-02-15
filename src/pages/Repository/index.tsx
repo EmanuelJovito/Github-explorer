680af9c447610e72c0912171308e9716aefa98e0
EmanuelJovito <emanuel_carneiro@hotmail.com>
2021-02-15 11:38:34 -0300

    feat: Criando navegação entre rotas

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import { useRouteMatch } from 'react-router-dom'
+
+interface RepositoyParams {
+  repository: string
+}
 
 const Repository: React.FC = () => {
-  return <h1>Repository</h1>
+  const { params } = useRouteMatch<RepositoyParams>()
+
+  return <h1>Repository:{params.repository}</h1>
 }
 
 export default Repository
